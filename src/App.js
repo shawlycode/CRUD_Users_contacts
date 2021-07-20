a6b0d27ee3d8a4968b4852cc5f1aa5b24fb5b391
Shawlycode <kutinaorlando@gmail.com>
2021-07-20 18:17:06 +0000

    added Math method and passed addNewUser as props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import States from './components/States'
-import './App.css';
+import React, { Component } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import Contacts from "./components/Contacts";
+import AddContactForm from "./components/AddContactForm";
 
-function App() {
-  return (
-    <div>
-    <States />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: []
+    }
+  }
+
+  addNewUser = (newData) => {
+    newData.id = Math.random().toString()
+    this.setState({
+      data: [...this.state.data, newData],
+    });
+  };
+  deleteUser = (id) => {
+    let savedUsers = this.state.data.filter((newData) => newData.id !==id);
+    this.setState({
+      data: savedUsers,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Container fluid style={{ marginTop: "2rem" }}>
+          <Row>
+            <Col md="4">
+              <AddContactForm addNewData={this.addNewUser} />
+            </Col>
+            <Col>
+              <Contacts
+                userData={this.state.data}
+                deleteUser={this.deleteUser}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
 }
 
 export default App;
