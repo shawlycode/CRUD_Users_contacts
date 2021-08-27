3dbbff4574437f2d707bbd95e610f826e9b51331
Shawlycode <kutinaorlando@gmail.com>
2021-08-27 19:49:29 +0000

    deleted reportWebVitals

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Store from './components/Store/Store';
+import store from './components/Store/Store';
 import {Provider} from 'react-redux';
 
 ReactDOM.render(
